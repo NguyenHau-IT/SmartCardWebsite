5468c3fc44d3322bc4b8e81206f00612ac57f8e7
Trương Hoàng Huy <truonghoanghuy164@gmail.com>
2025-10-30 22:10:40 +0700

    BQ4317-13 Story 2: Thống kê số thẻ được tạo trong tháng

---
+++ src/main/java/com/quizletclone/flashcard/model/QuizQuestion.java
@@ -0,0 +1,32 @@
+package com.quizletclone.flashcard.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "quiz_questions")
+public class QuizQuestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "flashcard_id", nullable = false)
+    private Flashcard flashcard;
+
+    @Column(name = "question_text", columnDefinition = "NVARCHAR(MAX)")
+    private String questionText;
+
+    @Column(name = "correct_answer", columnDefinition = "NVARCHAR(MAX)")
+    private String correctAnswer;
+}
